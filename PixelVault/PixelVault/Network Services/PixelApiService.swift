40ec9f29e29948698de9a8bc241b024d1f43bbb3
Abdulkarim Mziya <akmziya@Abdulkarims-MacBook-Air-2.local>
2026-03-17 05:17:24 -0700

    feat: add PixelApiService to fetch and decode posts from Pixabay API

--- PixelVault/PixelVault/Network Services/PixelApiService.swift
+++ PixelVault/PixelVault/Network Services/PixelApiService.swift
@@ -6,3 +6,48 @@
 //
 
 import Foundation
+
+class PixelApiService {
+    
+    enum HTTTPMethod: String {
+        case get = "GET"
+    }
+    
+    private func makeURL(searchQuery: String) -> URL? {
+        var components = URLComponents()
+        components.scheme = "https"
+        components.host = "pixabay.com"
+        components.path = "/api/"
+        
+        components.queryItems = [
+            URLQueryItem(name: "key", value: Secret.ApiKey),
+            URLQueryItem(name: "q", value: searchQuery),
+            URLQueryItem(name: "orientation", value: "vertical")
+        ]
+        
+        return components.url
+    }
+    
+    
+    func fetchAllPosts(with searchQuery: String?) async throws -> PixelAPIModel {
+        guard let query = searchQuery else {
+            throw AppError.noSearchQuery
+        }
+        
+        guard let url = makeURL(searchQuery: query) else {
+            throw AppError.invalidURL("Invalid url")
+        }
+        
+        let request = URLRequest(url: url)
+        
+        let data = try await NetworkHelper.shared.performTask(urlRequest: request)
+        
+        do {
+            let decoder = JSONDecoder()
+            return try decoder.decode(PixelAPIModel.self, from: data)
+        } catch {
+            throw AppError.decodingError(error)
+        }
+    }
+}
+
