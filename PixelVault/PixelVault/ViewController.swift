b8b162db6af3a91437cd471b7c81a7f38b2dce5c
Abdulkarim Mziya <akmziya@Abdulkarims-MacBook-Air-2.local>
2026-03-17 01:56:29 -0700

    chore: initial Xcode setup and removed storyboard for programmatic UI

---
+++ PixelVault/PixelVault/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PixelVault
+//
+//  Created by Abdulkarim Mziya on 2026-03-17.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
