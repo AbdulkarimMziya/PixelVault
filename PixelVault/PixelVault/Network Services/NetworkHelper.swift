e0ba78bf4ba2694b6c13592a8dc2a811f7d5e9ee
Abdulkarim Mziya <akmziya@Abdulkarims-MacBook-Air-2.local>
2026-03-17 03:51:19 -0700

    feat: create generic async network function to fetch JSON and image data

---
+++ PixelVault/PixelVault/Network Services/NetworkHelper.swift
@@ -0,0 +1,33 @@
+//
+//  NetworkHelper.swift
+//  PixelVault
+//
+//  Created by Abdulkarim Mziya on 2026-03-17.
+//
+
+import Foundation
+
+class NetworkHelper {
+    
+    static let shared = NetworkHelper()
+    
+    private var session: URLSession
+    
+    init() {
+        session = URLSession(configuration: .default)
+    }
+    
+    func performTask(urlRequest: URLRequest) async throws -> Data {
+        let (data,response) = try await session.data(for: urlRequest)
+        
+        guard let httpResponse = response as? HTTPURLResponse else {
+            throws AppError.noResponse
+        }
+        
+        guard (200...299).contains(httpResponse.statusCode) else {
+            throws AppError.badStatusCode(httpResponse.statusCode)
+        }
+        
+        return data
+    }
+}
