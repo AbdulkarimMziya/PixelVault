d927db116183da0f1cdb82bb3e50f1c5aa44e0d5
Abdulkarim Mziya <akmziya@Abdulkarims-MacBook-Air-2.local>
2026-03-17 15:41:51 -0700

    feat: add search bar UI and layout to ViewController

--- PixelVault/PixelVault/Controllers/ViewController.swift
+++ PixelVault/PixelVault/Controllers/ViewController.swift
@@ -8,12 +8,33 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    private let searchBar:UISearchBar = {
+        let searchBar = UISearchBar()
+        searchBar.placeholder = "Search for images"
+        searchBar.keyboardType = .default
+        searchBar.searchBarStyle = .minimal
+        searchBar.barTintColor = .systemCyan
+        searchBar.translatesAutoresizingMaskIntoConstraints = false
+        return searchBar
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .systemBackground
+        
+        view.addSubview(searchBar)
+        setupUI()
     }
-
+    
+    private func setupUI() {
+        
+        NSLayoutConstraint.activate([
+            searchBar.topAnchor.constraint(equalTo: view.topAnchor, constant: 48),
+            searchBar.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            searchBar.trailingAnchor.constraint(equalTo: view.trailingAnchor)
+        ])
+    }
 
 }
 
