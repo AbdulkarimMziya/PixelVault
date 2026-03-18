9ac66f5145165ef3dc7b6fd6f86eed7cd1b6938f
Abdulkarim Mziya <akmziya@Abdulkarims-MacBook-Air-2.local>
2026-03-18 04:03:56 -0700

    feat: add async image fetching method to PixelApiService for loading images from URLs

---
+++ PixelVault/PixelVault/Views/PostCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  PostCollectionViewCell.swift
+//  PixelVault
+//
+//  Created by Abdulkarim Mziya on 2026-03-18.
+//
+
+import UIKit
+
+class PostCollectionViewCell: UICollectionViewCell {
+    
+}
