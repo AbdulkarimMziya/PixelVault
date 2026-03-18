10da77061d9af4cc7b83de7ac9db6153bec19b2a
AbdulkarimMziya <66888226+AbdulkarimMziya@users.noreply.github.com>
2026-03-17 18:00:51 -0700

    Feature/search UI (#3)
    * feat: add search bar UI and layout to ViewController
    
    * feat: add search bar delegate and trigger API call on search button click
    
    * refactor: update PixelApiService to use static methods for API calls
    
    ---------
    
    Co-authored-by: Abdulkarim Mziya <akmziya@Abdulkarims-MacBook-Air-2.local>

--- PixelVault/PixelVault/Controllers/ViewController.swift
+++ PixelVault/PixelVault/Controllers/ViewController.swift
@@ -8,12 +8,47 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    private let searchBar:UISearchBar = {
+        let searchBar = UISearchBar()
+        searchBar.placeholder = "Search for images"
+        searchBar.keyboardType = .default
+        searchBar.searchBarStyle = .minimal
+        searchBar.barTintColor = .systemCyan
+        searchBar.translatesAutoresizingMaskIntoConstraints = false
+        return searchBar
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .systemBackground
+        
+        view.addSubview(searchBar)
+        searchBar.delegate = self
+        
+        setupUI()
     }
-
+    
+    private func setupUI() {
+        
+        
+        NSLayoutConstraint.activate([
+            searchBar.topAnchor.constraint(equalTo: view.topAnchor, constant: 48),
+            searchBar.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            searchBar.trailingAnchor.constraint(equalTo: view.trailingAnchor)
+        ])
+    }
+    
+    func performSearch(searchQuery: String) {
+        Task {
+            do {
+                let result = try await PixelApiService.fetchAllPosts(with: searchQuery)
+                print("Total Posts: \(result.hits.count)")
+            } catch {
+                print(error)
+            }
+        }
+    }
 
 }
 
