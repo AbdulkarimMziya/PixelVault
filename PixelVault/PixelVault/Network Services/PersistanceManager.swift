53e491f619ceab44ae32c81e1e21a4fa1ddcefca
Abdulkarim Mziya <akmziya@Abdulkarims-MacBook-Air-2.local>
2026-03-20 00:16:33 -0700

    feat: add PersistenceManager for handling document directory file paths

--- PixelVault/PixelVault/Network Services/PersistanceManager.swift
+++ PixelVault/PixelVault/Network Services/PersistanceManager.swift
@@ -6,3 +6,18 @@
 //
 
 import Foundation
+
+class PersistanceManager {
+    
+    static let shared = PersistanceManager()
+    
+    private init () {}
+    
+    func documentDirectory() -> URL {
+        return FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)[0]
+    }
+    
+    func fileFromDocumentDirectory(filename: String) -> URL {
+        documentDirectory().appending(path: filename)
+    }
+}
